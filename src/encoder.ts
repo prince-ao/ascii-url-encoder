a55d415cfff8f81be955801f1ba480f7f2f19f66
prince-ao <princead@tutanota.com>
2022-05-18 18:33:48 -0400

    add built-in functions

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,57 +1,12 @@
 export const encode = (str: string) => {
 	let res = "";
-	for (let i = 0; i < str.length; i++) {
-		switch (str[i]) {
-			case " ":
-				res += "%20";
-				break;
-			case "$":
-				res += "%24";
-				break;
-			case "&":
-				res += "%26";
-				break;
-			case "'":
-				res += "%27";
-				break;
-			case "(":
-				res += "%28";
-				break;
-			case ")":
-				res += "%29";
-				break;
-			case "+":
-				res += "%2B";
-				break;
-			case ",":
-				res += "%2C";
-				break;
-			case "/":
-				res += "%2F";
-				break;
-			case ":":
-				res += "%3A";
-				break;
-			case ";":
-				res += "%3B";
-				break;
-			case "<":
-				res += "%3C";
-				break;
-			case "=":
-				res += "%3D";
-				break;
-			case ">":
-				res += "%3E";
-				break;
-			case "?":
-				res += "3F";
-				break;
-			case "@":
-				res += "40";
-				break;
-			default:
-				res += str[i];
+	for (let char of encodeURIComponent(str)) {
+		if (char == "(") {
+			res += "%28";
+		} else if (char == ")") {
+			res += "%29";
+		} else {
+			res += char;
 		}
 	}
 	return res;
--- test/test.ts
+++ test/test.ts
@@ -17,10 +17,10 @@ describe("ASCII URL Encoder/Decoder", () => {
 		it("should return '%28Monumenta%20Vaticana%20Selecta%29%20Hugo%20Brandenburg%20-%20Le%20prime%20chiese%20di%20Roma.%20IV-VII%20secolo.%20Ediz.%20illustrata-Jaca%20Book%20%282021%29'", () => {
 			expect(
 				encode(
-					"(Monumenta Vaticana Selecta) Hugo Brandenburg - Le prime chiese di Roma. IV-VII secolo. Ediz. illustrata-Jaca Book (2021)"
+					"(Научно-популярная серия РФФИ) А. П. Черняев - Радиационные технологии_ наука, народное хозяйство, медицина-Изд-во Московского ун-та (2019)"
 				)
 			).to.equal(
-				"%28Monumenta%20Vaticana%20Selecta%29%20Hugo%20Brandenburg%20-%20Le%20prime%20chiese%20di%20Roma.%20IV-VII%20secolo.%20Ediz.%20illustrata-Jaca%20Book%20%282021%29"
+				"%28%D0%9D%D0%B0%D1%83%D1%87%D0%BD%D0%BE-%D0%BF%D0%BE%D0%BF%D1%83%D0%BB%D1%8F%D1%80%D0%BD%D0%B0%D1%8F%20%D1%81%D0%B5%D1%80%D0%B8%D1%8F%20%D0%A0%D0%A4%D0%A4%D0%98%29%20%D0%90.%20%D0%9F.%20%D0%A7%D0%B5%D1%80%D0%BD%D1%8F%D0%B5%D0%B2%20-%20%D0%A0%D0%B0%D0%B4%D0%B8%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D0%B8_%20%D0%BD%D0%B0%D1%83%D0%BA%D0%B0%2C%20%D0%BD%D0%B0%D1%80%D0%BE%D0%B4%D0%BD%D0%BE%D0%B5%20%D1%85%D0%BE%D0%B7%D1%8F%D0%B9%D1%81%D1%82%D0%B2%D0%BE%2C%20%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D0%B0-%D0%98%D0%B7%D0%B4-%D0%B2%D0%BE%20%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE%20%D1%83%D0%BD-%D1%82%D0%B0%20%282019%29"
 			);
 		});
 	});
